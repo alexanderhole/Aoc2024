ca29bb76e78ef70916fff18a886bb6797fb18231
alexanderhole <alexander.hole@and.digital>
2024-12-02 09:20:10 +0000

    Day 2 P2 test

--- Aoc2024/Days/2/Day2.cs
+++ Aoc2024/Days/2/Day2.cs
@@ -8,24 +8,40 @@ public class Day2() : BaseDay(2), IDay
     public int RunP1()
     {
         var rows = FileService.LoadLines();
-        return rows.Count(checkSafe);
+        var safeReports = 0;
         foreach (var row in rows)
         {
-            var safe = checkSafe(row);
+            var levels = row.Split(" ").Select(x => int.Parse(x)).ToArray();
+            var (safe, badReport) = checkSafe(levels);
+            if (!safe)
+            {
+                var firstList = levels.ToList();
+                firstList.RemoveAt(badReport);
+                var firstCheck = checkSafe(firstList.ToArray());
+                if (!firstCheck.safe)
+                {
+                    var secondList = levels.ToList();
+                    secondList.RemoveAt(badReport + 1);
+                    var secondCheck = checkSafe(secondList.ToArray());
+                    if(!secondCheck.safe) continue;
+                }
+            }
+
+            safeReports += 1;
         }
+        return safeReports;
     }
 
-    private bool checkSafe(string row)
+    private (bool safe,int badReport) checkSafe(int[] levels)
     {
-        var levels = row.Split(" ").Select(x => int.Parse(x)).ToArray();
         if (levels[0] == levels[1])
-            return false;
+            return (false,0);
         if (levels[0] > levels[1])
         {
             for (int i = 0; i < levels.Length -1; i++)
             {
                 if (levels[i] < levels[i + 1] || (levels[i] - levels[i + 1]) > 3 || levels[i] == levels[i + 1])
-                    return false;
+                    return (false,i);
             }
         }
         if (levels[0] < levels[1])
@@ -33,12 +49,14 @@ public class Day2() : BaseDay(2), IDay
             for (int i = 0; i < levels.Length -1; i++)
             {
                 if (levels[i] > levels[i + 1] || (levels[i + 1] - levels[i]) > 3 || levels[i] == levels[i + 1])
-                    return false;
+                    return (false,i);
             }
         }
 
-        return true;
+        return (true,0);
     }
+    
+    
 
     public int RunP2()
     {
