eafeb91a58850ea4197bf5640d6c5db77675fdb9
alexanderhole <alexander.hole@and.digital>
2024-12-08 10:50:38 +0000

    Day 8 p2

--- Aoc2024/Days/8/Day8.cs
+++ Aoc2024/Days/8/Day8.cs
@@ -52,11 +52,43 @@ public class Day8() : BaseDay(8), IDay
     {
         return Math.Abs(nr1 - nr2);
     }
+    
     public dynamic RunP2()
     {
-        
-        return 0;
-    }
+        var grid = FileService.LoadGrid();
+        List<GridCoord> antennas = new List<GridCoord>();
+        HashSet<(int, int)> antiNodes = new HashSet<(int, int)>();
+        foreach (var item in grid.items)    
+        {
+            if (item.value != '.')
+            {
+                antennas.Add(item);
+                antiNodes.Add((item.Coord.x, item.Coord.y));
+            }
+        }
 
+        foreach (var antennaTypes in antennas.GroupBy(x => x.value))
+        {
+            foreach (var antennaType in antennaTypes)
+            {
+                var others = antennaTypes.Where(x => x != antennaType);
+                foreach (var other in others)
+                {
+                    var findDistancex = antennaType.Coord.x- other.Coord.x;
+                    var findDistancey = antennaType.Coord.y- other.Coord.y;
+                    var currentX = findDistancex + antennaType.Coord.x;
+                    var currentY = findDistancey + antennaType.Coord.y;
+                    while (currentX <= grid.maxX && currentX >= 0
+                                                 && currentY >= 0 && currentY <= grid.maxY)
+                    {
+                        antiNodes.Add((currentX,currentY));
+                        currentX = findDistancex + currentX;
+                        currentY = findDistancey + currentY;
+                    }
+                }
+            }
+        }
+        return antiNodes.Where(x => x.Item1 >= 0 && x.Item2 >= 0 && x.Item1 < grid.maxX && x.Item2 < grid.maxY).Count();
+    }
     
 }
