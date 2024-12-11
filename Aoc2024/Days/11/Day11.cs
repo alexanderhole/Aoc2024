4bf3fb56f4efd1cc02d8c2c01e2d5fceb7e87df3
alexanderhole <alexander.hole@and.digital>
2024-12-11 09:58:25 +0000

    Day 11 p2 - bad

--- Aoc2024/Days/11/Day11.cs
+++ Aoc2024/Days/11/Day11.cs
@@ -24,8 +24,9 @@ public class Day11() : BaseDay(11), IDay
             foreach (var stone in stones)
             {
                 var applyRules = ApplyRules(stone);
-                if(applyRules.Any(x => x<0)) Debugger.Break();
-                tempOrder.AddRange(applyRules);
+                tempOrder.Add(applyRules.Item1.Value);
+                if(applyRules.Item2 != null)
+                    tempOrder.Add(applyRules.Item2.Value);
             }
 
             stones = tempOrder;
@@ -33,23 +34,44 @@ public class Day11() : BaseDay(11), IDay
         return stones.Count();
     }
 
-    private IEnumerable<long> ApplyRules(long stone)
+    private (long?,long?) ApplyRules(long stone)
     {
-        var returnValue = new List<long>();
         if (stone == 0)
-            return new List<long>() { 1 };
-        else if (stone.ToString().Length % 2 == 0)
+            return (1,null);
+        else
         {
-            var charsEnumerable = stone.ToString().Chunk(stone.ToString().Length / 2);
-            var enumerable = charsEnumerable.Select(x => long.Parse(new string(x)));
-            return enumerable.ToList();
+            var s = stone.ToString();
+            if (s.Length % 2 == 0)
+            {
+                var mid = s.Length / 2;
+                return (long.Parse(s.Substring(0,mid)),long.Parse(s.Substring(mid)));
+            }
+            else 
+                return (stone * 2024,null);
         }
-        else 
-            return new List<long>() { stone * 2024 };
     }
 
     public dynamic RunP2()
   {
-      return 0;
+      var stones = FileService.LoadFile().Split(" ").Select(long.Parse);
+      int blinks = 75;
+      Stopwatch sw = new Stopwatch();
+      sw.Start();
+      for (int i = 0; i < blinks; i++)
+      {
+          sw.Restart();
+          var tempOrder = new List<long>();
+          foreach (var stone in stones)
+          {
+              var applyRules = ApplyRules(stone);
+              tempOrder.Add(applyRules.Item1.Value);
+              if(applyRules.Item2 != null)
+                  tempOrder.Add(applyRules.Item2.Value);
+          }
+
+          stones = tempOrder;
+          Console.WriteLine(i + " blink time " + sw.ElapsedMilliseconds + "ms" + " with " + stones.Count() +" items");
+      }
+      return stones.Count();
   }
 }
