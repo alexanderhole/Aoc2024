55910c32f489e61c0aab0dc813db699afd522834
alexanderhole <alexander.hole@and.digital>
2024-12-11 13:54:33 +0000

    Day 11 p2 - fixed,, lru caches ey

--- Aoc2024/Days/11/Day11.cs
+++ Aoc2024/Days/11/Day11.cs
@@ -1,10 +1,12 @@
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq.Expressions;
+using System.Reflection.Emit;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using Aoc2024.Days._1;
 using Aoc2024.Interfaces;
+using LRU;
 using Microsoft.VisualBasic.CompilerServices;
 using SG.Utilities;
 
@@ -12,10 +14,10 @@ namespace Aoc2024.Days._2;
 
 public class Day11() : BaseDay(11), IDay
 {
+    private LeastRecentlyUsedCache<(long stone, int depth), long> blinkCache;
 
     public dynamic RunP1()
     {
-        
         var stones = FileService.LoadFile().Split(" ").Select(long.Parse);
         int blinks = 25;
         for (int i = 0; i < blinks; i++)
@@ -24,72 +26,76 @@ public class Day11() : BaseDay(11), IDay
             foreach (var stone in stones)
             {
                 var applyRules = ApplyRules(stone);
-                tempOrder.Add(applyRules.Item1.Value);
-                if(applyRules.Item2 != null)
+                tempOrder.Add(applyRules.Item1);
+                if (applyRules.Item2 != null)
                     tempOrder.Add(applyRules.Item2.Value);
             }
 
             stones = tempOrder;
         }
+
         return stones.Count();
     }
 
-    private (long?,long?) ApplyRules(long stone)
+    private (long, long?) ApplyRules(long stone)
     {
         if (stone == 0)
-            return (1,null);
+            return (1, null);
         else
         {
             var digitsLog10 = Digits_Log10(stone);
             if (digitsLog10 % 2 == 0)
             {
-                var pow = Math.Pow(10, digitsLog10/2);
+                var pow = Math.Pow(10, digitsLog10 / 2);
                 var d = stone / pow;
                 var one = Math.Floor(d);
                 var two = Math.Round(d % 1 * pow);
                 return ((long)one, (long)two);
             }
-            else 
-                return (stone * 2024,null);
+            else
+                return (stone * 2024, null);
         }
     }
 
     public dynamic RunP2()
-  {
-      var stones = FileService.LoadFile().Split(" ").Select(long.Parse);
-      int blinks = 40;
-      Stopwatch sw = new Stopwatch();
-      sw.Start();
-      sw.Restart();
-      var stonecounter = 0;
-      var final = new List<long>();
-      foreach (var stone in stones)
-      {
-          stonecounter += 1;
-          var tempOrder = new List<long>(){stone};
+    {
+        var stones = FileService.LoadFile().Split(" ").Select(long.Parse);
+        int blinks = 75;
+        long final = 0;
+        blinkCache = new LeastRecentlyUsedCache<(long stone, int depth), long>(500000000);
+        foreach (var stone in stones)
+        {
+            final += GetChildrenCount(blinks, stone);
+        }
 
-          for (int i = 0; i < blinks; i++)
-          {
-              var ruleOrder = new List<long>();
-              foreach (var temp in tempOrder)
-              {
-                  var applyRules = ApplyRules(temp);
-                  ruleOrder.Add(applyRules.Item1.Value);
-                  if (applyRules.Item2 != null)
-                      ruleOrder.Add(applyRules.Item2.Value);
-              }
-              Console.WriteLine(stonecounter + " stone time " + i +" blink "   + sw.ElapsedMilliseconds + "ms" + " with " + tempOrder.Count() +" items");
+        return final;
+    }
 
-              tempOrder = ruleOrder;
-          }
+    private long GetChildrenCount(int blinks, long stone)
+    {
+        if (blinks == 0)
+            return 1;
+        blinks -= 1;
+        long counter = 0;
+        var rules = ApplyRules(stone);
+        long childrenCount = 0;
+        if (blinkCache.ContainsKey((stone, blinks)))
+            childrenCount = blinkCache[(stone, blinks)];
+        else
+        {
+            childrenCount = GetChildrenCount(blinks, rules.Item1);
+            blinkCache[(stone, blinks)] = childrenCount;
+        }
 
-          //stones = tempOrder;
-          Console.WriteLine(stonecounter + " stone time " + sw.ElapsedMilliseconds + "ms" + " with " + stones.Count() +" items");
-          final.AddRange(tempOrder);
-      }
-      return final.Count();
-  }
+        counter += childrenCount;
+        if (rules.Item2 != null)
+        {
+            counter += rules.Item2 != null ? GetChildrenCount(blinks, rules.Item2.Value) : 0;
+        }
+
+        return counter;
+    }
+
     public static int Digits_Log10(long n) =>
         n == 0L ? 1 : (n > 0L ? 1 : 2) + (int)Math.Log10(Math.Abs((double)n));
-
 }
