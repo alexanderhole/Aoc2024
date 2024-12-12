06cf4309d3fdc0f3533372e0fdf20728384d9685
alexanderhole <alexander.hole@and.digital>
2024-12-12 10:41:07 +0000

    Day 12 P2

--- Aoc2024/Days/12/Day12.cs
+++ Aoc2024/Days/12/Day12.cs
@@ -19,9 +19,7 @@ public class Day12() : BaseDay(12), IDay
                 if(processed.Contains(items[x, y])) continue;
                 var returnSet = new HashSet<GridCoord>();
                 var block = GetBlock(items[x, y], grid, returnSet);
-                var bigFence = FindBigFencePieces(block, grid);
-                var count = bigFence * block.Count();
-                fenceCount += count;
+                fenceCount += block.Sum(x => x.FenceCount) * block.Count();
                 processed.UnionWith(block);
             }
         }
@@ -35,33 +33,18 @@ public class Day12() : BaseDay(12), IDay
         fenceCount += LowerFence(block);
         fenceCount += RightFence(block);
         fenceCount += LeftFence(block);
-
         return fenceCount;
-        var distinctXs = block.DistinctBy(x => x.Coord.x).Select(x => x.Coord.x).ToList();
-        for (int x = 0; x < distinctXs.Count(); x++)
-        {
-            var row = block.Where(coord => coord.Coord.x == distinctXs[x]).Select(x => x.Coord.y).Order().ToList();
-            for (int i = 0; i < row.Count()-1; i++)
-            {
-                if (row[i] + 1 != row[i + 1])
-                {
-                    fenceCount += 1;
-                }
-            }
-        }
-
-        return fenceCount;return 0;
     }
     private static int RightFence(HashSet<GridCoord> block)
     {
         var rightFences = block.Count(x => x.fences.right);
         var distinctXs = block.DistinctBy(x => x.Coord.x).Select(x => x.Coord.x).ToList();
-        for (int x = 0; x < distinctXs.Count(); x++)
+        for (var x = 0; x < distinctXs.Count(); x++)
         {
             var itemsWithUpperFence =
                 block.Where(gridCoord => gridCoord.Coord.x == distinctXs[x] && gridCoord.fences.right).OrderBy(x => x.Coord.y).ToList();
             
-            for (int i = 0; i < itemsWithUpperFence.Count()-1; i++)
+            for (var i = 0; i < itemsWithUpperFence.Count()-1; i++)
             {
                 if (itemsWithUpperFence[i].Coord.y + 1 == itemsWithUpperFence[i + 1].Coord.y)
                 {
@@ -127,69 +110,72 @@ public class Day12() : BaseDay(12), IDay
                 }
             }
         }
-
         return lowerFences;
     }
 
     private HashSet<GridCoord> GetBlock(GridCoord item, Grid grid, HashSet<GridCoord> returnSet)
     {
-        //while (true)
+        returnSet.Add(item);
+        var neighbours = CheckNeighbours(item, grid);
+        neighbours.ExceptWith(returnSet);
+        returnSet.UnionWith(neighbours);
+        foreach (var neighbour in neighbours)
         {
-            var originalCount = returnSet.Count();
-            returnSet.Add(item);
-            var neighbours = CheckNeighbours(item, grid);
-            neighbours.ExceptWith(returnSet);
-            returnSet.UnionWith(neighbours);
-            foreach (var neighbour in neighbours)
-            {
-                returnSet.UnionWith(GetBlock(neighbour, grid, returnSet));
-            }
+            returnSet.UnionWith(GetBlock(neighbour, grid, returnSet));
         }
-
         return returnSet;
     }
 
     private HashSet<GridCoord> CheckNeighbours(GridCoord item, Grid grid)
     {
         var ret = new HashSet<GridCoord>();
-        var fenceCount = 4;
         var coord = grid.GetDown(item);
         if (coord?.value == item.value)
         {
-            Add(ret, coord);
+            ret.Add(coord);
             item.fences.bottom = false;
         }
         coord = grid.GetRight(item);
         if(coord?.value == item.value)
         {
-            Add(ret, coord);
+            ret.Add(coord);
             item.fences.right = false;
         }
         coord = grid.GetUp(item);
         if(coord?.value == item.value)
         {
-            Add(ret, coord);
+            ret.Add(coord);
             item.fences.up = false;
         }
         coord = grid.GetLeft(item);
         if(coord?.value == item.value)
         {
-            Add(ret, coord);
+            ret.Add(coord);
             item.fences.left = false;
         }
-        
         return ret;
-    }
-
-    private static bool Add(HashSet<GridCoord> ret, GridCoord coord)
-    {
-        //coord.fences -= 1;
-        return ret.Add(coord);
     }
 
     public dynamic RunP2()
     {
-        return 0;
+        var grid = FileService.LoadGrid();
+        var items = grid.items;
+        var processed = new HashSet<GridCoord>();
+        var fenceCount = 0;
+        for (int x = 0; x < grid.maxX; x++)
+        {
+            for (int y = 0; y < grid.maxY; y++)
+            {
+                if(processed.Contains(items[x, y])) continue;
+                var returnSet = new HashSet<GridCoord>();
+                var block = GetBlock(items[x, y], grid, returnSet);
+                var bigFence = FindBigFencePieces(block, grid);
+                var count = bigFence * block.Count();
+                fenceCount += count;
+                processed.UnionWith(block);
+            }
+        }
+        return fenceCount;
     }
 
 }
